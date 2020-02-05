3f121af2b3869a649d9541e6b4816e5794e45dee
Joo YeJin Kelly <yejinkellyjoo@gmail.com>
2020-02-05 12:46:54 +0900

    make java

---
+++ src/main/java/yjkellyjoo/CommitMessageScraperApplication.java
@@ -0,0 +1,13 @@
+package yjkellyjoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitMessageScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitMessageScraperApplication.class, args);
+	}
+
+}
